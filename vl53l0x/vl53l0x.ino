9fce6dbc4e9833f54abccb3d4fb66089e538ef34
Aatreya97 <a_chak09@hotmail.com>
2018-12-07 15:33:22 -0500

    vl53l0x code
    instantiate new objects for each sensor

--- vl53l0x/vl53l0x.ino
+++ vl53l0x/vl53l0x.ino
@@ -5,7 +5,9 @@ const int XS_SENS1 = 30;
 const int XS_SENS2 = 31;
 const int XS_SENS3 = 32;
 
-Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox1 = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox2 = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox3 = Adafruit_VL53L0X();
 
 void setup() {
   Serial.begin(115200);
@@ -25,7 +27,23 @@ void setup() {
   digitalWrite(XS_SENS2, LOW);
   digitalWrite(XS_SENS3, LOW);
   delay(10);
-  
+
+  digitalWrite(XS_SENS1, HIGH);
+  if (!lox1.begin(0x30)) {
+    Serial.println(F("Failed to boot VL53L0X Sens 1"));
+    while(1);
+  }
+  digitalWrite(XS_SENS2, HIGH);
+  if (!lox2.begin(0x31)) {
+    Serial.println(F("Failed to boot VL53L0X Sens 2"));
+    while(1);
+  }
+  digitalWrite(XS_SENS3, HIGH);
+  if (!lox3.begin(0x35)) {
+    Serial.println(F("Failed to boot VL53L0X Sens 3"));
+    while(1);
+  }
+    
   // power 
   Serial.println(F("VL53L0X API Ranging\n\n")); 
 }
@@ -33,48 +51,30 @@ void setup() {
 
 void loop() {
   
-  // SENSOR 1 INITIALIZATION AND DATA GRABBING
-  digitalWrite(XS_SENS1, HIGH);
-  if (!lox.begin(0x30)) {
-    Serial.println(F("Failed to boot VL53L0X Sens 1"));
-    while(1);
-  }
+  // SENSOR 1 DATA GRABBING
   VL53L0X_RangingMeasurementData_t measure_sens1;
-  lox.rangingTest(&measure_sens1, false); // pass in 'true' to get debug data printout!
+  lox1.rangingTest(&measure_sens1, false); // pass in 'true' to get debug data printout!
   if (measure_sens1.RangeStatus != 4) {  // phase failures have incorrect data
     Serial.print("Sens1(mm): "); Serial.print(measure_sens1.RangeMilliMeter); Serial.print(" | ");
   } else {
     Serial.print("Sens1(mm): "); Serial.print("**"); Serial.print(" | ");
   }
-  digitalWrite(XS_SENS1, LOW);
   
-  // SENSOR 2 INITIALIZATION AND DATA GRABBING
-  digitalWrite(XS_SENS2, HIGH);
-  if (!lox.begin(0x31)) {
-    Serial.println(F("Failed to boot VL53L0X Sens 2"));
-    while(1);
-  }
+  // SENSOR 2 DATA GRABBING
   VL53L0X_RangingMeasurementData_t measure_sens2;
-  lox.rangingTest(&measure_sens2, false);
+  lox2.rangingTest(&measure_sens2, false);
   if (measure_sens2.RangeStatus != 4) {  // phase failures have incorrect data
     Serial.print("Sens2(mm): "); Serial.print(measure_sens2.RangeMilliMeter); Serial.print(" | ");
   } else {
     Serial.print("Sens1(mm): "); Serial.print("**"); Serial.print(" | ");
   }
-  digitalWrite(XS_SENS2, LOW);
 
-  // SENSOR 3 INITIALIZATION AND DATA GRABBING
-  digitalWrite(XS_SENS3, HIGH);
-  if (!lox.begin(0x32)) {
-    Serial.println(F("Failed to boot VL53L0X Sens 3"));
-    while(1);
-  }
+  // SENSOR 3 DATA GRABBING
   VL53L0X_RangingMeasurementData_t measure_sens3;
-  lox.rangingTest(&measure_sens3, false);
+  lox3.rangingTest(&measure_sens3, false);
   if (measure_sens3.RangeStatus != 4) {  // phase failures have incorrect data
     Serial.print("Sens3(mm): "); Serial.print(measure_sens3.RangeMilliMeter); Serial.println(" ");
   } else {
     Serial.print("Sens1(mm): "); Serial.print("**"); Serial.println(" ");
   }
-  digitalWrite(XS_SENS3, LOW);
 }
