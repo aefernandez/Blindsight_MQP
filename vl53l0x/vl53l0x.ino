689691a5bbc13bd78645a461eb8ad663be4ca779
Aatreya97 <a_chak09@hotmail.com>
2018-12-08 17:17:19 -0500

    vl53lox.ino
    Move mesureSens variables to globals

--- vl53l0x/vl53l0x.ino
+++ vl53l0x/vl53l0x.ino
@@ -9,6 +9,10 @@ Adafruit_VL53L0X lox1 = Adafruit_VL53L0X();
 Adafruit_VL53L0X lox2 = Adafruit_VL53L0X();
 Adafruit_VL53L0X lox3 = Adafruit_VL53L0X();
 
+VL53L0X_RangingMeasurementData_t measure_sens1;
+VL53L0X_RangingMeasurementData_t measure_sens2;
+VL53L0X_RangingMeasurementData_t measure_sens3;
+
 void setup() {
   Serial.begin(115200);
 
@@ -50,9 +54,7 @@ void setup() {
 
 
 void loop() {
-  
   // SENSOR 1 DATA GRABBING
-  VL53L0X_RangingMeasurementData_t measure_sens1;
   lox1.rangingTest(&measure_sens1, false); // pass in 'true' to get debug data printout!
   if (measure_sens1.RangeStatus != 4) {  // phase failures have incorrect data
     Serial.print("Sens1(mm): "); Serial.print(measure_sens1.RangeMilliMeter); Serial.print(" | ");
@@ -61,7 +63,6 @@ void loop() {
   }
   
   // SENSOR 2 DATA GRABBING
-  VL53L0X_RangingMeasurementData_t measure_sens2;
   lox2.rangingTest(&measure_sens2, false);
   if (measure_sens2.RangeStatus != 4) {  // phase failures have incorrect data
     Serial.print("Sens2(mm): "); Serial.print(measure_sens2.RangeMilliMeter); Serial.print(" | ");
@@ -70,7 +71,6 @@ void loop() {
   }
 
   // SENSOR 3 DATA GRABBING
-  VL53L0X_RangingMeasurementData_t measure_sens3;
   lox3.rangingTest(&measure_sens3, false);
   if (measure_sens3.RangeStatus != 4) {  // phase failures have incorrect data
     Serial.print("Sens3(mm): "); Serial.print(measure_sens3.RangeMilliMeter); Serial.println(" ");
